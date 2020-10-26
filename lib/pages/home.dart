d0400f9c15a1e4e2872312478e501e808ddaf244
nicolo <nicolo.festa90@gmail.com>
2020-10-26 11:46:14 +0100

    Share functionality restored.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:age_calculator/pages/calculator_partial_screen.dart';
 import 'package:age_calculator/pages/my_dates_partial_screen.dart';
 import 'package:age_calculator/pages/settings_partial_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class RoutePopupMenuItem {
   RoutePopupMenuItem({this.title, this.icon, this.route});
@@ -68,26 +69,21 @@ class _HomeState extends State<Home> {
     return list;
   }
 
-  // _share(BuildContext context) {
-  //   final RenderBox box = context.findRenderObject();
-  //   String message =
-  //       "https://play.google.com/store/apps/details?id=com.festa.age_calculator";
-  //   if (lifeTime != null) {
-  //     message = "I'm ${lifeTime.days} days old.\n" +
-  //         "My next birthday is in ${age.daysToNextBD} days.\n\n" +
-  //         message;
-  //   }
-  //   Share.share(message,
-  //       subject: 'Age Calculator',
-  //       sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
-  // }
+  _share(BuildContext context) {
+    final RenderBox box = context.findRenderObject();
+    String message = "Try the Age Calculator!\n" +
+        "https://play.google.com/store/apps/details?id=com.festa.age_calculator";
+    Share.share(message,
+        subject: 'Age Calculator',
+        sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
+  }
 
   _menuSelect(BuildContext context, RoutePopupMenuItem choice) async {
     if (choice.title == 'Settings') {
       Navigator.pushNamed(context, choice.route);
     }
     if (choice.title == 'Share') {
-      //_share(context);
+      _share(context);
     }
   }
 
